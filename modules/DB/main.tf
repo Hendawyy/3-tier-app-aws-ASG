e5e4cef90334e8d1fcc5186e707d169e6bc1bf29
Hendawyy <seifhendawy1@gmail.com>
2025-01-19 18:25:58 +0200

    final snapshot

--- modules/DB/main.tf
+++ modules/DB/main.tf
@@ -4,14 +4,18 @@ resource "aws_db_subnet_group" "dbsubnet" {
 }
 
 resource "aws_db_instance" "mysql" {
-  allocated_storage      = 20
-  engine                 = "mysql"
-  engine_version         = "5.7.44"
-  instance_class         = "db.t3.micro"
-  username               = "admin"
-  password               = "adminpassword"
-  parameter_group_name   = "default.mysql5.7"
-  multi_az               = true
-  vpc_security_group_ids = [var.security_group_ids]
-  db_subnet_group_name   = aws_db_subnet_group.dbsubnet.name
+  allocated_storage         = 20
+  engine                    = "mysql"
+  engine_version            = "5.7.44"
+  instance_class            = "db.t3.micro"
+  username                  = "admin"
+  password                  = "adminpassword"
+  parameter_group_name      = "default.mysql5.7"
+  multi_az                  = true
+  vpc_security_group_ids    = [var.security_group_ids]
+  db_subnet_group_name      = aws_db_subnet_group.dbsubnet.name
+  skip_final_snapshot       = false
+  final_snapshot_identifier = "my-final-snapshot"
+
+
 }
